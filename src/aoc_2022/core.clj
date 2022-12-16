e7d203022bd29d48adcfe4da21e2b6dab5aa3c83
Thomas Marquis <thomas.marquis.dev@gmail.com>
2022-12-16 22:43:04 +0100

    day 1 done

--- src/aoc_2022/core.clj
+++ src/aoc_2022/core.clj
@@ -1,7 +1,23 @@
 (ns aoc-2022.core
   (:gen-class))
 
+(require '[clojure.string :as str])
+
+(defn read-data []
+   (map (fn [elf-cal] (map #(Integer. %)
+                       (str/split elf-cal #"\n")))
+    (str/split
+    (slurp "resources/elve_calories") #"\n\n")))
+
+(defn sum-vector [vect]
+  (reduce #(+ %1 %2) 0 vect))
+
+(defn day1 []
+  (println (sum-vector 
+            (take 3
+            (sort > (map sum-vector
+                      (read-data)))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (day1))
--- src/aoc_2022/day1.clj
+++
@@ -1,5 +0,0 @@
-(ns aoc-2022.day1)
-
-(defn -main
-  [& args]
-  (println "coucou"))
