f78dfeae0dd73ce8323947b8689f5703046339e2
Thomas Marquis <thomas.marquis.dev@gmail.com>
2022-12-16 23:42:58 +0100

    day2 part2 done

--- src/aoc_2022/core.clj
+++ src/aoc_2022/core.clj
@@ -30,60 +30,32 @@
   "A => pierre
    B => papier
    C => siceaux
-   X => p1 loose
+   X => p2 loose
    Y => draw
    Z => p1 win"
   (cond
-    (and (= p1 "A") (= p2 "X")) {:result :draw
-                                 :p1 (+ 3 1) :p2 (+ 3 1)}
+    (and (= p1 "A") (= p2 "X")) {:result :p1
+                                 :p1 (+ 6 3) :p2 3}
     (and (= p1 "B") (= p2 "Y")) {:result :draw
                                  :p1 (+ 3 2) :p2 (+ 3 2)}
-    (and (= p1 "C") (= p2 "Z")) {:result :draw
-                                 :p1 (+ 3 3) :p2 (+ 3 3)}
+    (and (= p1 "C") (= p2 "Z")) {:result :p2
+                                 :p1 3 :p2 (+ 6 1)}
 
-    (and (= p1 "A") (= p2 "Y")) {:result :p2
-                                 :p1 1 :p2 (+ 6 2)}
+    (and (= p1 "A") (= p2 "Y")) {:result :draw
+                                 :p1 (+ 3 1) :p2 (+ 3 1)}
     (and (= p1 "B") (= p2 "X")) {:result :p1
                                  :p1 (+ 6 2) :p2 1}
 
-    (and (= p1 "A") (= p2 "Z")) {:result :p1
-                                 :p1 (+ 6 1) :p2 3}
-    (and (= p1 "C") (= p2 "X")) {:result :p2
-                                 :p1 3 :p2 (+ 6 1)}
+    (and (= p1 "A") (= p2 "Z")) {:result :p2
+                                 :p1 1 :p2 (+ 6 2)}
+    (and (= p1 "C") (= p2 "X")) {:result :p1
+                                 :p1 (+ 6 3) :p2 2}
 
     (and (= p1 "B") (= p2 "Z")) {:result :p2
                                  :p1 2 :p2 (+ 6 3)}
-    (and (= p1 "C") (= p2 "Y")) {:result :p1
-                                 :p1 (+ 6 3) :p2 2}
+    (and (= p1 "C") (= p2 "Y")) {:result :draw
+                                 :p1 (+ 3 3) :p2 (+ 3 3)}
     :else (throw (Exception. (str "invalid combination: " p1 p2)))))
-
-;; (defn get- [p1 p2]
-;;   "X => loose
-;;    Y => draw
-;;    Z => win"
-;;   (cond
-;;     (and (= p1 "A") (= p2 "X")) {:result :draw
-;;                                  :p1 (+ 3 1) :p2 (+ 3 1)}
-;;     (and (= p1 "B") (= p2 "Y")) {:result :draw
-;;                                  :p1 (+ 3 2) :p2 (+ 3 2)}
-;;     (and (= p1 "C") (= p2 "Z")) {:result :draw
-;;                                  :p1 (+ 3 3) :p2 (+ 3 3)}
-
-;;     (and (= p1 "A") (= p2 "Y")) {:result :p2
-;;                                  :p1 1 :p2 (+ 6 2)}
-;;     (and (= p1 "B") (= p2 "X")) {:result :p1
-;;                                  :p1 (+ 6 2) :p2 1}
-
-;;     (and (= p1 "A") (= p2 "Z")) {:result :p1
-;;                                  :p1 (+ 6 1) :p2 3}
-;;     (and (= p1 "C") (= p2 "X")) {:result :p2
-;;                                  :p1 3 :p2 (+ 6 1)}
-
-;;     (and (= p1 "B") (= p2 "Z")) {:result :p2
-;;                                  :p1 2 :p2 (+ 6 3)}
-;;     (and (= p1 "C") (= p2 "Y")) {:result :p1
-;;                                  :p1 (+ 6 3) :p2 2}
-;;     :else (throw (Exception. (str "invalid combination: " p1 p2)))))
 
 (defn day2 []
   (println (let [data (read-rps-strategy-data)]
